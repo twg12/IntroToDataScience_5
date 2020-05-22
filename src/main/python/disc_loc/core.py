a3263eaab7ae7efe86b67ee84f494fb030254198
HANK <hankpkj@gmail.com>
2020-05-23 05:18:21 +0900

    HANK: convert jupyter into python code

--- src/main/python/disc_loc/core.py
+++ src/main/python/disc_loc/core.py
@@ -1,26 +1,89 @@
 import pandas as pd
-import numbers
-import math
+import requests
+from bs4 import BeautifulSoup
 from scipy.spatial import distance
 from haversine import haversine
 import geopandas
 import folium
 
+
+
+
 class Discrimination_Location:
-    def __init__(self, x: float = 37, y: float = 128):
+    def __init__(self, dist):
         """
         :param x: x coordinate
         :param y: y coordinate
         """
-        self.x_coord = x
-        self.y_coord = y
+        self.url_hpid = "http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEmrrmRltmUsefulSckbdInfoInqire?\
+        serviceKey=rdmH5XR5ycW9emHC9D56bNydZrvc6546Z0fxlqCdCPc4RpWZ99GMoGoRIKRabGJKz2WFcTwN9ekSTfCvzzhiYA%\
+        3D%3D&pageNo=1&numOfRows=402"
+        self.url_location = ""
+
+        # TODO: store and load from s3
         self.df = pd.read_csv('C:\\Users\\park\\Desktop\\location.csv', encoding='euc-kr')
 
+    def call_api_hpid(self):
+        """
+            get hpid list using html parser
+        :return: hpid list
+        """
+        req = requests.get(self.url_hpid)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
 
-    def process(self):
-        print(self.df)
-        print(self.df.dtypes)
+        # Get hpid list
+        hpid = soup.find_all('hpid')
+        hpid_list = list(map(lambda x: x.text, hpid))
+        return hpid_list
 
+    def call_api_location(self, hpid_list):
+        """
+            get location about hospital by hpid
+            return : pd.DataFrame
+            ( hpid, hname, x_coord, y_coord )
+        """
+        df = pd.DataFrame(columns=['hpid', 'hname', 'x_coord', 'y_coord'])
+
+        for pid in hpid_list:
+            url = 'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytBassInfoInqire?\
+            serviceKey=rdmH5XR5ycW9emHC9D56bNydZrvc6546Z0fxlqCdCPc4RpWZ99GMoGoRIKRabGJKz2WFcTwN9ekSTfCvzzhiYA%3D%3D&\
+            HPID=' + pid + '&pageNo=1&numOfRows=1&'
+            req = requests.get(url)
+            html = req.text
+            soup = BeautifulSoup(html, 'html.parser')
+            hpid = soup.find('hpid').text
+            hname = soup.find('dutyname').text
+            x_coord = soup.find('wgs84lat').text
+            y_coord = soup.find('wgs84lon').text
+
+            df = df.append({'hpid': hpid,
+                            'hname': hname,
+                            'x_coord': x_coord,
+                            'y_coord': y_coord
+                            }, ignore_index=True)
+
+        # for calculating , declare dtype float
+        df[['x_coord', 'y_coord']] = df[['x_coord', 'y_coord']].astype('float64')
+
+        return df
+
+    def df_to_csv(self, df:pd.DataFrame , path, file_name, encoding='euc-kr'):
+
+        df.to_csv(path + '\\' + '{}.csv'.format(file_name) ,encoding=encoding)
+
+    def load_target(self):
+
+        # TODO: next day
+        pass
+def process(self):
+        """
+            1. Api call for hpid
+            2. Api call for location using hpid
+            3. load target.csv (Administrative area)
+            4. calculate distance ( given number )
+            5. visualization using folium
+        """
 
         def func(x):
             print(haversine(x, (37.49794, 127.02758)))
